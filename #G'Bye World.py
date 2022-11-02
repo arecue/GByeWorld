d28e647f98651bb5f68245cf5c6ee412fdbee5fc
arecue <project2@denworth.com>
2022-11-01 21:26:07 -0400

    asdfasdf

--- #G'Bye World.py
+++ #G'Bye World.py
@@ -1,3 +1,14 @@
-#G'Bye World
-random_text = "xyz"
-print("Goodbye World" + " " + random_text)
+# G'Bye World
+import random
+import string
+ 
+def random_string_generator(str_size, allowed_chars):
+    return ''.join(random.choice(allowed_chars) for x in range(str_size))
+ 
+chars = string.ascii_letters
+size = 3
+ 
+print(chars)
+print('Random String of length 12 =', random_string_generator(size, chars))
+
+print("Goodbye World" + " " + random_string_generator(size, chars))
